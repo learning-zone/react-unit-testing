6193afe57124294cb24c497f8b721b032dda8f1b
Pradeep Kumar <pradeep.vwa@gmail.com>
2022-08-08 20:33:53 +0530

    Update Header.test.js

--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -1,38 +1,21 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import { render, fireEvent, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import Header from './Header';
+afterEach(cleanup);
 
-/**
- * shallow():- method is used to render the single component that we are testing. 
- * It does not render child components.
- */
-
-/**
- * mount():- method renders the full DOM including the child components of the parent 
- * component that we are running the tests.
- */
-
-/**
- * render():- method has the ability to render to static HTML.
- * It renders the children.
- * But this does not have access to React lifecycle methods.
- */
 describe('Header Component', () => {
 
     let wrapper
     beforeEach(() => {
-        wrapper = render(<Header />);
+        wrapper = render(<Header/>)
     })
-
+    
     test('should check the menu icon', () => {
-       const component = screen.getByTestId("menu-icon");
-       expect(component);
+       expect(screen.getByTestId("menu-icon")).toBeInTheDocument();
     })
 
     test('should check the header component title', () => {
-        const title = screen.getByTestId("menu-title").text;
-       expect(title).toMatch('React Unit Testing with Jest and Enzyme')
+       expect(screen.getByText('React Unit Testing with Jest and Enzyme')).toBeInTheDocument();
     })
 
 })
